adc0ba17d48486dd19ec7f6a6b88701d7e50ef61
GrundeFAA <aasen.grunde@gmail.com>
2022-10-28 23:17:22 +0200

    test med schedule

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import { scheduleJob } from "node-schedule";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const jobs: { date: string }[] = [];
+
+scheduleJob("*/2 * * * *", () => {
+  jobs.push({ date: new Date().toLocaleString("en-GB") });
+});
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 
 app.get("/", async (req, res) => {
-  res.json(".");
+  res.json(jobs);
 });
 
 app.listen(port, () => {
