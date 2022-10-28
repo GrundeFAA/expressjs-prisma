80a3ca75ee2158d32c58572159cf5a5b70c18930
GrundeFAA <aasen.grunde@gmail.com>
2022-10-28 23:27:06 +0200

    ...

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import { scheduleJob } from "node-schedule";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const jobs: { date: string }[] = [];
+const jobs: { date: string; numberOfJobs: number }[] = [];
 
-scheduleJob("*/2 * * * *", () => {
-  jobs.push({ date: new Date().toLocaleString("en-GB") });
+scheduleJob("0 */2 * * *", () => {
+  jobs.push({
+    numberOfJobs: jobs.length,
+    date: new Date().toLocaleString("en-GB"),
+  });
 });
 
 app.use(express.json());
